99b09bd8ac62f168bf01fa8d0fa27ec8b068cbd7
Sebastian <sebgertz@gmail.com>
2019-04-29 12:49:55 -0400

    add eventlistener to submit function that draws basic grid

--- designs.js
+++ designs.js
@@ -3,8 +3,22 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+const form = document.getElementById("sizePicker");
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  makeGrid();
+})
 
+function makeGrid() {
+  const canvas = document.getElementById("pixelCanvas");
+  for (let i = 0; i < 3; i++) {
+    let row = document.createElement("tr");
+    canvas.appendChild(row)
+    for (let j = 0; j < 3; j++) {
+      let cell = document.createElement("td");
+      canvas.appendChild(cell)
+    }
+  }
 // Your code goes here!
 
 }
